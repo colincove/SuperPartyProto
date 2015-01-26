db0cddd5a919f034f58e66e1e78a884d36d81110
colincove <cove.colin@gmail.com>
2015-01-25 20:41:56 -0800

    starting new level design.

--- ProtoShooter/ProtoShooter/src/prefabs/caverns.prefab.js
+++ ProtoShooter/ProtoShooter/src/prefabs/caverns.prefab.js
@@ -5,9 +5,9 @@
     renderers:[
     ],
     triggers:{cavern:{type:BodyTypes.BOX,
-        config:{collisionGroup:"cavern", width:1000, height:1000, transform:{position:{x:-400, y:-1300}}}}, 
+        config:{collisionGroup:"cavern", width:(65 - 35)*levelTileSize, height:8*levelTileSize - 20, transform:{position:{x:levelTileSize*34, y:0}}}}, 
             cavern2:{type:BodyTypes.BOX,
-        config:{collisionGroup:"cavern", width:1000, height:1000, transform:{position:{x:800, y:0}}}}
+        config:{collisionGroup:"cavern", width:1000, height:3000, transform:{position:{x:-500, y:-3500}}}}
     },
     mappings:{}
 }
